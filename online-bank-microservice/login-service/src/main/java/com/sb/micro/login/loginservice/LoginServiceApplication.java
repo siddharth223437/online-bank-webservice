bccf38598558b4f9e875f8f55df582260f04e797
sid <sid@192.168.1.12>
2018-10-14 16:55:17 -0500

    added demo application

--- online-bank-microservice/login-service/src/main/java/com/sb/micro/login/loginservice/LoginServiceApplication.java
+++ online-bank-microservice/login-service/src/main/java/com/sb/micro/login/loginservice/LoginServiceApplication.java
@@ -2,8 +2,14 @@ package com.sb.micro.login.loginservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan("com.sb.micro.login.vo")
+@EnableTransactionManagement
+@ComponentScan(basePackages="com.sb.micro.login.*")
 public class LoginServiceApplication {
 
 	public static void main(String[] args) {
--- online-bank-microservice/login-service/src/main/java/com/sb/micro/login/vo/UsersVO.java
+++ online-bank-microservice/login-service/src/main/java/com/sb/micro/login/vo/UsersVO.java
@@ -1,7 +1,5 @@
 package com.sb.micro.login.vo;
 
-import javax.persistence.Entity;
-
-@Entity
 public class UsersVO {
+
 }
