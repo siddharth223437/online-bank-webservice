95f87873fac3d25c4100c9fb4527907c1c926c26
Pragati Tanwar <43156513+PragatiTanwar06@users.noreply.github.com>
2018-10-15 23:33:49 -0400

    Updated UsersVO attributes and created AddressVO

---
+++ online-bank-microservice/login-service/src/main/java/com/sb/micro/login/vo/AddressVO.java
@@ -0,0 +1,43 @@
+package com.sb.micro.login.vo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class AddressVO {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int addressId;
+	String city,country,state;
+	@ManyToOne
+	private UsersVO userVO;
+	
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+}
--- online-bank-microservice/login-service/src/main/java/com/sb/micro/login/vo/UsersVO.java
+++ online-bank-microservice/login-service/src/main/java/com/sb/micro/login/vo/UsersVO.java
@@ -1,17 +1,46 @@
 package com.sb.micro.login.vo;
-import java.sql.Time;
+
+import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import org.hibernate.annotations.UpdateTimestamp;
 
 
 @Entity
 public class UsersVO
 {
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	int userId;
 	String firstName,lastName,mobileNumber,createdBy;
-	Time lastUpdated;
-	
+	boolean isAvailable;
+	@UpdateTimestamp
+	LocalDateTime lastUpdated;
+	@OneToMany (mappedBy="userVO")
+	private List<AddressVO> addressVO; 
+
+	public List<AddressVO> getAddressVO() {
+		return addressVO;
+	}
+	public void setAddressVO(List<AddressVO> addressVO) {
+		this.addressVO = addressVO;
+	}
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	public LocalDateTime getLastUpdated() {
+		return lastUpdated;
+	}
+	public void setLastUpdated(LocalDateTime lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
 	public int getUserId() {
 		return userId;
 	}
@@ -34,19 +63,12 @@ public class UsersVO
 		return mobileNumber;
 	}
 	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
+		this.mobileNumber = mobileNumber; 
 	}
 	public String getCreatedBy() {
 		return createdBy;
 	}
 	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
-	}
-	public Time getLastUpdated() {
-		return lastUpdated;
-	}
-	public void setLastUpdated(Time lastUpdated) {
-		this.lastUpdated = lastUpdated;
-	}
-	
+	}	
 }
